82ad481621ea9801fd6f5bea2df8b2dabc732fe7
frank <ffavela@gmail.com>
2021-01-28 15:46:29 -0600

    combinatorial helper functions

--- mset_lib/core.py
+++ mset_lib/core.py
@@ -1,3 +1,8 @@
+from math import factorial
+import operator as op
+from functools import reduce
+import sys
+
 def tOp(xStr, LL):
     return [[xStr] + e for e in LL]
 
@@ -122,3 +127,19 @@ def C(L,k):
             return 1
         return c(nL(L),k-1,i+1)+c(L[1:],k,i+L[0])
     return c(L,k)
+
+#############HELPER FUNCTIONS######################
+
+def ncr(r,n):
+    #Important condition
+    if r > n or n < 0:
+        return 0
+    r = min(r, n-r)
+    numer = reduce(op.mul, range(n, n-r, -1), 1)
+    denom = reduce(op.mul, range(1, r+1), 1)
+    return numer // denom
+
+def myComb(k,N):
+    if k > N or N < 0:
+        return 0
+    return int(factorial(N)/(factorial(k)*factorial(N-k)))
