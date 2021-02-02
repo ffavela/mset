1cc667170815817e81332dda8ac7707a147b41b4
ffavela <ffavela@gmailcom>
2021-02-02 13:08:04 -0600

    better form for comparing with itertools

--- mset_lib/miscellaneous.py
+++ mset_lib/miscellaneous.py
@@ -15,3 +15,32 @@ def myComb(k,N):
     if k > N or N < 0:
         return 0
     return int(factorial(N)/(factorial(k)*factorial(N-k)))
+
+def dumbCompare(x,y):
+    """ Surely there is a better way but it furfills the intended purpose"""
+    for ex in x:
+        if ex not in y:
+            print("ex", ex)
+            return False
+    for ey in y:
+        if ey not in x:
+            print("ey", ey)
+            return False
+    return True
+
+def getMsetForm2(L):
+    d={}
+    for e in L:
+        if e not in d:
+            d[e]=0
+        d[e]+=1
+    M=[ d[e]*[e] for e in d]
+    M.sort(key=len, reverse=True)
+    return M
+
+def getUniqueL(L):
+    uL=[]
+    for e in L:
+        if e not in uL:
+            uL.append(e)
+    return uL
--- mset_mod/itercomp.py
+++ mset_mod/itercomp.py
@@ -4,32 +4,53 @@ import timeit
 import sys
 sys.path.append('../')
 from mset_lib.core import *
+from mset_lib.miscellaneous import *
 
 
 def getAllIterComb(S,k):
-    return [e for e in it.combinations(S, k)]
+    return [list(e) for e in it.combinations(S, k)]
 
 testcode = '''
 import itertools as it
 def getAllIterComb(S,k):
     return [e for e in it.combinations(S, k)]
 
-S="frankyrox"
+S="mississippi"
 k=3
 getAllIterComb(S,k)
 '''
 
 print(timeit.timeit('output = 10*5'))
 
-print(timeit.timeit(stmt = testcode))
+# print(timeit.timeit(stmt = testcode))
 # for e in it.combinations("frankyrox", 3):
 #     print(e)
 
-S="frankyrox"
+S="mississippi"
+S=sorted(list(S))
+print(S)
 k=5
 
 R=getAllIterComb(S,k)
 print(len(R))
-print(len(set(R)))
 
-print(len(zG(S,k)))
+# print(len(set(R))) #Unhashable in the current form
+
+myR=zG(S,k)
+print(len(myR))
+
+print(dumbCompare(myR, R))
+
+print(myR[0], R[0])
+
+M = getMsetForm2(S)
+
+print(M)
+
+uL=getUniqueL(R)
+
+print(len(uL))
+
+combM=MG(M,k)
+print(len(combM))
+
